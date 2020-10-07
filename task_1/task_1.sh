1e43355277b23ffb813bb7d30e4d6b0434f97882
Ritesh Ramkhelawon <riteshramkhelawon@c02d909emd6m.lan>
2020-10-07 12:19:18 +0100

    [task1] updated file creation and removal

---
+++ task_1/task_1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+MyVar="this is \"my variable\""
+
+echo $MyVar
+
+echo ./task_1.sh $SHELLOPTS
+
+echo 'shellopts variable is ' $1
+
+touch myfile\'.file
+
+rm myfile\'.file
+
+exit 0
