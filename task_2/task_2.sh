6ae5e99b5df1d6705fc6116a83fc798bbdca9c44
Ritesh Ramkhelawon <riteshramkhelawon@c02d909emd6m.lan>
2020-10-07 13:49:53 +0100

    [task 2] refactored if statement

--- task_2/task_2.sh
+++ task_2/task_2.sh
@@ -1,9 +1,9 @@
-//1
-remainder=$(( 73 % 8 ))
-echo remainder
+#1
+remainder=$(( 73%8 ))
+echo remainder of 73/8 is $remainder
 
 
-//2
+#2
 result1=$(( result1=4**6 ))
 result2=$(( result2=5**3 ))
 
@@ -11,16 +11,12 @@ finalResult=$(( $result1+$result2 ))
 
 echo $finalResult
 
-
-//3
+#3
 echo singleLineTask=$(( singleLineTask=(4**6)+(5**3) ))
 
 
-//4
-num1=$0
-num2=$1
-
-if [$num1 == $num2 ]; then
+#4
+if(($1 == $2)); then
 echo 'numbers are the same'
 else
 echo 'numbers are different'
