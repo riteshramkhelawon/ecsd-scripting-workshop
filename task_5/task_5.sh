86f8abb6c3f65261a12fd511ccc0c3f38d4f3726
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-09 15:14:48 +0100

    [Task 5] Changed while loop into for loop for final task

--- task_5/task_5.sh
+++ task_5/task_5.sh
@@ -1,33 +1,22 @@
 #! /usr/bin/env bash
 
 #1
-#systemPassword="pass"
-#
-#while [[ $userPassword != $systemPassword ]]; do
-#	echo -n "Enter password:"
-#	read userPassword
-#done		
-#
-#echo "correct password"
-#
-#exit 0
+systemPassword="pass"
+while [[ $userPassword != $systemPassword ]]; do
+	echo -n "Enter password:"
+	read userPassword
+done		
+echo "correct password"
 
 #2
 echo -n "Enter range start number:"
 read rangeStart
-
 echo -n "Enter range end number:"
 read rangeEnd
-
 total=0
-count=$rangeStart
-while [[ $count <= $rangeEnd ]]; do
-	$tempTotal = count ** 2
-	total = $total+tempTotal
-	$count = $((count+1))
+for i in $(seq $rangeStart $rangeEnd)
+do
+	total=$(( $total+(i**2) ))
 done
-
 echo $total
-
 exit 0
-
