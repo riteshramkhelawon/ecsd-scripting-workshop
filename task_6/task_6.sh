38c8e66b5929e04f13eef625cc77fa9163fe85b7
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-09 16:22:26 +0100

    [Task 6] Improved readability of code

--- task_6/task_6.sh
+++ task_6/task_6.sh
@@ -7,20 +7,16 @@ echo "replace the character"
 read replaceChar
 echo "replace for character"
 read replaceFor
-
 echo "new sentence is  ${myArray[@]//$replaceChar/$replaceFor}"
 
 #2
 declare -a websiteArr
 websiteArr=('www.google.com' 'www.facebook.com')
 declare -a latencies
-  
 for i in "${websiteArr[@]}"
 do
-	echo $i
-       #tempvar=$(ping -c 5 $i | grep "round-trip" | cut -f5 -d "/")
+	echo "pinging $i..."
 	latency=$(ping -c 5 -q $i | cut -d "/" -s -f5)
-       latencies+=("$latency")          
+	latencies+=("$latency")          
 done
-
 echo "latencies: ${latencies[*]}"
