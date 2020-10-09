cd5bee8442da2d87d5eab464c1eb12e4e6cf313d
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-09 16:17:35 +0100

    [Task 6] Implemented Arrays task 2

--- task_6/task_6.sh
+++ task_6/task_6.sh
@@ -8,4 +8,19 @@ read replaceChar
 echo "replace for character"
 read replaceFor
 
-echo ${myArray[@]//$replaceChar/$replaceFor}
+echo "new sentence is  ${myArray[@]//$replaceChar/$replaceFor}"
+
+#2
+declare -a websiteArr
+websiteArr=('www.google.com' 'www.facebook.com')
+declare -a latencies
+  
+for i in "${websiteArr[@]}"
+do
+	echo $i
+       #tempvar=$(ping -c 5 $i | grep "round-trip" | cut -f5 -d "/")
+	latency=$(ping -c 5 -q $i | cut -d "/" -s -f5)
+       latencies+=("$latency")          
+done
+
+echo "latencies: ${latencies[*]}"
